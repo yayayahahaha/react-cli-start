55fe612194327d5f9ecb0614605f4d1733ada00f
flyc <yayayahahahaooii@gmail.com>
2021-07-27 00:15:24 +0800

    實作了一個有suspense 狀態，應該就是loading 時要呈現的component 的那種功能

--- src/components/SiteLayout.js
+++ src/components/SiteLayout.js
@@ -1,3 +1,27 @@
+const createResource = function(promise) {
+  let error = null
+  let response = null
+  let isThen = false
+  promise.then(r => { response = r; isThen = true }).catch(e => { error = e })
+  return {
+    read: function() {
+      console.count('in read function') // have no idea why it triggered third-times
+
+      if (error) throw error
+      if (isThen) return response
+      throw promise
+    }
+  }
+}
+const doPromise = () => new Promise(resolve => setTimeout(() => resolve(), 3000))
+
+const readInfo = createResource(doPromise())
+
+function Status() {
+  const status = readInfo.read()
+  return <p>this is status {status}</p>
+}
+
 function SiteLayout(props) {
   const {
     children = <div>default children</div>,
@@ -6,6 +30,7 @@ function SiteLayout(props) {
 
   return (
     <div className="site-container">
+      <Status></Status>
       <div>{Menu}</div>
       <div>{children}</div>
     </div>
